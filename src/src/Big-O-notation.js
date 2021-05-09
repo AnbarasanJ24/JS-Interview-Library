a1192ba472be0ac992970a2b55c92c1292d35ffb
Anbarasan <46339443+AnbarasanJ24@users.noreply.github.com>
2021-05-09 19:52:32 +0530

    Day 2
    Added Logarithm def, space complexity def and recap details.

--- src/src/Big-O-notation.js
+++ src/src/Big-O-notation.js
@@ -1,11 +1,13 @@
 // 1. Why Big O
     // In case of many solutions to the problem, we can select the best one using Big O
     // We can count number of operation computer has to perform
+    // when the input grows then the time ef execution will also grow
 // 2. To find better
     // Faster, Less memory (how many varibale or function created)
 // 3. Without Big O
     // we cannot mesaure using time it will vary based on computer
-
+// 4. Efficient Big O
+    // O(1), O(log N), O(N), O(N log N), O(N^2)
 
 
     function sumNumbers(number){
