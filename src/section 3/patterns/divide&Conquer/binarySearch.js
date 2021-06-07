72dd68d07971d6f97ed753414e64f947050afe0b
Anbarasan <46339443+AnbarasanJ24@users.noreply.github.com>
2021-06-07 22:25:10 +0530

    Divide & Conqueor

--- src/section 3/patterns/divide&Conquer/binarySearch.js
+++ src/section 3/patterns/divide&Conquer/binarySearch.js
@@ -1,6 +1,6 @@
 // Create a function which take sorted array and value to be find in the array. 
 // In case of available return the index else -1
-let arr = [1, 2, 3, 4, 5, 6];
+let arr = [1, 23, 4];
 let searchValue = 4;
 
 // Naive solution
@@ -21,19 +21,28 @@ const binarySearch = (arr, searchValue) => {
     }
     // Initialize left and right value
     let left = 0, right = arr.length - 1;
-
     // Loop till left less than right 
     // Calculate middle using left and right 
     // if middle equal to searchValue then return index
     // if search value less than middle then search will be in left side
     // if search value greater than middle then search will be in right side
-    // while (left < right) {
-    //     let middle = (left + right) / 2;
-    //     if (arr[middle] === searchValue)
-    //         return middle;
-    //     else if (searchValue )
+    while (left <= right) {
+        let middle = Math.floor((right + left) / 2);
 
-    // }
+        if (arr[middle] === searchValue)
+            return middle;
+        else if (searchValue < arr[middle]) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
 
-    // return -1;
+    return -1;
 }
+
+console.log(binarySearch(arr, searchValue))
+
+// Time complexity O (Log N)
+// Space complexity O (1)
---
+++ src/section 4/recursion/introduction.md
@@ -0,0 +1,10 @@
+# What is recursion
+
+    # Function clling itself again with some condition change in parameter
+    # Once it reach the base case then functin callling itself
+
+# Examples
+
+    # JSON.parse and JSON.stringfy
+    # Document get element by id and DOM traversal
+    # Object traversal
