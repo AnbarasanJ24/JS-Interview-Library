d38877f59d2c13f6f8d9622101430030e87637a5
Anbarasan <anbarasanj24@gmail.com>
2021-06-29 09:18:07 +0530

    Added Linked list methods

--- src/Data Structure/Singley Linked List/singlyLinkedList.js
+++ src/Data Structure/Singley Linked List/singlyLinkedList.js
@@ -126,10 +126,13 @@ class SinglyLinkedList {
     get(index) {
 
         // corner case 
-        if (index < 0 || index >= this.length) {
-            return null;
-        }
+        if (index < 0 || index >= this.length) return null;
 
+        // Create counter and initialize current node 
+        // Keep updating current node untill index equal to current
+        // Once matched update the current node value
+        // let current = this.head;
+        // let counter = 0;
         let counter = 0;
         let current = this.head;
 
@@ -142,6 +145,131 @@ class SinglyLinkedList {
         return current;
     }
 
+    // Set Element at the passed index 
+    set(index, value) {
+
+        // Base case 
+        if (index < 0 || index >= this.length) return null;
+
+        //  use get function
+        let current = this.get(index);
+        current.value = current ? value : null;
+
+        // Return new node
+        return current;
+
+    }
+
+    // Insert new node at the specific position
+    insert(index, value) {
+
+        // Corner case, return null if index less than zero and 
+        // greater then length
+        // If index is zero then use unshift
+        // If index match with the length then use push
+        if (index < 0 || index > this.length) return false;
+        if (index === 0 || previous === 0) return !!this.unshift(value);
+        if (index === this.length || next === this.length) return !!this.push(value);
+
+
+        // Create new node & Get current and previous node 
+        let newNode = new Node(value);
+        let previous = this.get(index - 1);
+        let next = this.get(index);
+        previous.next = newNode;
+        newNode.next = next;
+
+
+        // return boolean
+        this.length++;
+        return true;
+    }
+
+    // Remove element from the list for give index
+    // Skipping comments because this function same like insert process
+    remove(index) {
+
+        // Corner case
+        // If index is zero then use shift
+        // If index is length minus -1 then use pop
+        if (index < 0 || index >= this.length) return false;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        // Get current and next node
+        // Point current next to next node 
+        let previous = this.get(index - 1);
+        let removed = previous.next;
+        previous.next = removed.next;
+
+        // return boolean 
+        this.length--;
+        return true;
+    }
+
+    // Print all values in list
+    print() {
+        if (!this.head) return null;
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+
+    // Reverse the linked list - 
+    // Naive Time complexity -> O (N) and space complexity O (N)
+    reverse() {
+
+        // Base case
+        if (!this.head) return null;
+
+        // Loop the linked list and collect value in array and reverse it
+        // Loop the same linked list and update the value from array
+        let current = this.head;
+        let values = [];
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+
+        let index = values.length;
+        current = this.head;
+        while (current) {
+            current.value = values[--index];
+            current = current.next;
+        }
+
+        return this;
+    }
+
+    // Reverse In place - Efficient 
+    // Time complexity
+    // Space complexity 
+
+    reverseInplace() {
+        // 1->2->3->Null
+        //  P  C  
+        // Null<-1<- 2<- 3
+
+        // Corner case
+        if (!this.head) return null;
+
+        // Create current,previous Nodes
+        // iterate the loop
+        // Take next back up and move current pointer to previous
+        // move current and previous for next node iteration
+        let current = this.head;
+        this.head = this.tail;
+        let previous = null;
+        while (current) {
+            let next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return this;
+    }
 
 }
 
@@ -152,12 +280,11 @@ list.push("How are you");
 // let removed = list.pop();
 // console.log("removed", removed)
 // list.pop();
-// list.pop();
-// list.pop();
 // list.shift();
-// list.shift();
-// list.shift();
-// list.shift();
-list.unshift("Good Morning")
-let getList = list.get(5);
-console.log("List", getList)
+// list.insert(0, "Good Morning")
+// list.set(7, "Anbu")
+// let getList = list.get(2);
+// list.remove(2);
+let reversedList = list.reverseInplace();
+console.log("List", reversedList.print());
+
