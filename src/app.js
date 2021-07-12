21c6ee01b32a09b50c3d81ef251944b3d0e6e998
Anbarasan <anbarasanj24@gmail.com>
2021-07-12 22:28:29 +0530

    Added Singly Linked list Methods

--- src/app.js
+++ src/app.js
@@ -26,8 +26,7 @@ class DoublyLinkedList {
         // If exists make the new node as tail
         // else Insert the element next to the tail
         if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
+            this.head = this.tail = newNode;
         } else {
             this.tail.next = newNode;
             newNode.prev = this.tail;
@@ -62,11 +61,68 @@ class DoublyLinkedList {
         this.length--;
         return lastItem;
     }
+
+    // Remove element from the start - Time Complexity O (1)
+    shift() {
+
+        // Base case 
+        if (!this.head) return undefined;
+
+        // Core logic
+        let oldHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+
+        // return new node
+        this.length--;
+        return oldHead;
+    }
+
+    // Unshift - Add element at the first -Time complexity is O (1)
+    unShift(value) {
+
+        let newNode = new Node(value);
+
+        // Logic
+        if (!this.head) {
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+
+        // return list
+        this.length++;
+        return this;
+
+    }
+
+    // Get Elemnet at given index - Time complexity O (N)
+    // Index starts from zero
+    get(index) {
+
+        // base case
+        if (index < 0 || index === this.length) return undefined;
+
+        // let middle = Math.floor(this.length/2);
+        // let result = index <= middle ? getIndexedNode(this.head)
+
+        // return node
+    }
 }
 
 let dll = new DoublyLinkedList();
-// dll.push(5);
-// dll.push(4);
+dll.push(5);
+dll.push(4);
 dll.push("Last Item");
-dll.pop()
+dll.unShift("First")
+dll.unShift("Second")
 console.log("DLL", dll)
