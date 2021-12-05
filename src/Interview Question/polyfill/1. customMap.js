5361f521d1d665eb9791cafbc277fda3a3ae1e45
Anbarasan <anbarasanj24@gmail.com>
2021-12-05 18:44:06 +0530

    Added customMap polyfill

---
+++ src/Interview Question/polyfill/1. customMap.js
@@ -0,0 +1,34 @@
+/**
+ * The map() method returns a new array, where it will call a callback function on each value
+ * const numbers = [1,2,3];
+ * const result = numbers.map(num => num *2)
+ * o/p => [2,4,6]
+ */
+
+// Notes
+// It will return an array
+// It will call a function on every value in the array 
+// Before call the function check  element exits in array 
+// Each function will take three arguments, num, index, numbers
+
+Array.prototype.customeMap = function (callback) {
+
+    if (typeof callback !== 'function') {
+        throw new Error("Invalid function");
+    }
+
+    let sourceArray = this;
+    let result = new Array(sourceArray.length);
+
+    for (let index = 0; index <= sourceArray.length; index++) {
+        if (index in sourceArray) {
+            result[index] = callback(sourceArray[index], index, sourceArray);
+        }
+    }
+
+    return result;
+}
+
+const numbers = [1, 2, 3];
+const result = numbers.customeMap(num => num * 2)
+console.log(result);
