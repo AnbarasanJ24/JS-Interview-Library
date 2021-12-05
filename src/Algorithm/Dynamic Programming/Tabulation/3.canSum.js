ae1a36638df7977661063b8cc186e0a85098ff87
Anbarasan <anbarasanj24@gmail.com>
2021-12-05 13:24:11 +0530

    Added can sum problem

---
+++ src/Algorithm/Dynamic Programming/Tabulation/3.canSum.js
@@ -0,0 +1,34 @@
+/**
+ * Given a target sum and array of numbers. Check target sum can be achived using array of number even using multiple times
+ * Array of number will not negative
+ * Return true if its possible else false
+ * targetsum = 7
+ * numbers = [5,3,4,7]
+ */
+// Notes => Here target sum is reducing the problem (Table based on target sum)
+// Since boolean will be return, we can take false as default value
+// Seed tivial value => targetSum = 0
+// Check current value is true then further value index will be true 
+
+const canSum = (targetSum, numbers) => {
+    const table = Array(targetSum + 1).fill(false);
+    table[0] = true;
+
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i]) {
+            for (let num of numbers) {
+                table[i + num] = true;
+            }
+        }
+    }
+    console.log(table);
+    return table[targetSum];
+}
+
+console.log(canSum(7, [2, 3]))
+// console.log(canSum(7, []))
+// console.log(canSum(8, [2, 3, 5]))
+// console.log(canSum(300, [7, 14]))
+
+// T = O(numbers * targetSum)
+// S = O(targetSum)
--- src/Algorithm/Dynamic Programming/Tabulation/introduction.md
+++ src/Algorithm/Dynamic Programming/Tabulation/introduction.md
@@ -5,3 +5,11 @@
     => For table we are using array, it should be always extra one space than requirement 
        (For Ex: fib(6) => Needs array of 0 to 6 which is 7 in length)
     => Fill the table with zero for counting problem 
+
+# Tabulation Recipe
+    => Visualize the problem as a table (Input which is reducing the problem)
+    => size of the table is based on input+1 
+    => initialize the table with default values
+    => seed the trivial answer to the table
+    => iterate through the table
+    => fill further position based on current position (Think Recursive ways & convert it into iterative)
