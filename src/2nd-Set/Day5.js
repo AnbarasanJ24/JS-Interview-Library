8168db74004478399ae6e2664e4162ae90b56062
Anbarasan <anbarasanj24@gmail.com>
2022-02-11 08:05:25 +0530

    Added Diagonal Sum

---
+++ src/2nd-Set/Day5.js
@@ -0,0 +1,46 @@
+
+// Given a square matrix mat, return the sum of the matrix diagonals.
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Input: mat = [[1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]]
+// Output: 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+
+// Notice that element mat[1][1] = 5 is counted only once.
+
+// https://leetcode.com/problems/matrix-diagonal-sum/
+
+var diagonalSum = function (mat) {
+
+    if (mat.length < 0) return -1;
+    let diagonalSum = 0;
+
+    // Handle (0,0),(1,1),(2,2)
+    for (let i = 0; i < mat.length; i++) {
+        diagonalSum += mat[i][i];
+    }
+
+    // Handle (0,2),(1,1),(2,0)
+    for (let i = 0, j = mat.length - 1; i < mat.length; i++, j--) {
+        diagonalSum += mat[i][j];
+    }
+
+    // Remove middle value in case of odd rows
+    if (mat.length % 2 === 1) {
+        diagonalSum -= mat[Math.floor(mat.length / 2)][Math.floor(mat.length / 2)];
+    }
+
+    return diagonalSum;
+};
+
+// public int diagonalSum(int[][] mat) {
+//         int res = 0;
+//         int n = mat.length;
+//     for (int i = 0; i < n; i++) {
+//         res += mat[i][i]; // Primary diagonals are row = column!
+//         res += mat[n - 1 - i][i]; // Secondary diagonals are row + column = n-1!
+//     }
+//     return n % 2 == 0 ? res : res - mat[n / 2][n / 2]; // if n is a odd number, that mean we have added the center element twice!
+// }
