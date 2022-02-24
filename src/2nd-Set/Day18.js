7875f9f8121a76fde3416933b353a6226a2ef80c
Anbarasan <anbarasanj24@gmail.com>
2022-02-24 22:18:58 +0530

    Added Palindrome

---
+++ src/2nd-Set/Day18.js
@@ -0,0 +1,23 @@
+// Palindrome Number
+
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
+// For example, 121 is a palindrome while 123 is not.
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+var isPalindrome = function (x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let number = x;
+    let reverse = 0;
+    while (number > 0) {
+        reverse = reverse * 10 + number % 10;
+        number = parseInt(number / 10);
+    }
+    return x === reverse;
+};
