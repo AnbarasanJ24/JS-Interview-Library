426dc298401312ab6107e07b45b456c28f75eac8
Anbarasan J <anbu@anbus-MacBook-Air.local>
2022-06-13 22:03:59 +0530

    Added Promise Ployfill

--- src/Interview Question/polyfill/11.customMemoize.js
+++ src/Interview Question/polyfill/11.customMemoize.js
@@ -6,5 +6,6 @@ function factorial(n) {
     }
     return factorial(n-1) * n; 
  }
-
  console.log(factorial(25))
+ 
+ 
--- src/Interview Question/polyfill/7.customPromise.js
+++ src/Interview Question/polyfill/7.customPromise.js
@@ -7,41 +7,97 @@
 // console.log(add(5, 5));
 
 // To convert any function to promise then return promise instance
-function add(a, b) {
-    return new Promise((resolve, reject) => {
-        if (a < 0 || b < 0) {
-            reject(new Error('Number should not be negative!'));
-            return;
-        }
-        resolve(a + b);
-    })
-}
+// function add(a, b) {
+//     return new Promise((resolve, reject) => {
+//         if (a < 0 || b < 0) {
+//             reject(new Error('Number should not be negative!'));
+//             return;
+//         }
+//         resolve(a + b);
+//     })
+// }
 
-add(5, 5)
-    .then(response => {
-        console.log("Addition of two numbers is ", response);
-    })
-    .catch(error => {
-        console.log("Error occured");
-        console.log(error);
-    })
+// add(5, 5)
+//     .then(response => {
+//         console.log("Addition of two numbers is ", response);
+//     })
+//     .catch(error => {
+//         console.log("Error occured");
+//         console.log(error);
+//     })
 
 
-// Create a function which wait for 1sec
+// // Create a function which wait for 1sec
 
-function wait(ms) {
-    return new Promise((resolve, reject) => {
+// function wait(ms) {
+//     return new Promise((resolve, reject) => {
 
-        if (ms <= 0) {
-            reject(new Error('Can"t wait for given seconds'));
-            return;
-        }
+//         if (ms <= 0) {
+//             reject(new Error('Can"t wait for given seconds'));
+//             return;
+//         }
+//         setTimeout(() => {
+//             resolve(ms);
+//         }, ms);
+//     })
+// }
+
+// wait(0)
+//     .then(response => console.log(`The function is waited for ${response} seconds`))
+//     .catch(error => console.log(error))
+
+
+// Create a custom Promise
+
+// we need to create custome promise step by step 
+// Need to have custom fetch so that we can return our custom promise
+// Promise class will be created with callback function
+
+const myFetch = () => {
+    return new MyPromise((resolve, reject) => {
         setTimeout(() => {
-            resolve(ms);
-        }, ms);
+            reject('Promise Resolved')
+        }, 1000);
     })
 }
 
-wait(0)
-    .then(response => console.log(`The function is waited for ${response} seconds`))
-    .catch(error => console.log(error))
+class MyPromise {
+    constructor(executorFunction) {
+        this.promiseChain = [];
+        this.handleError = () => {};
+        this.onResolve = this.onResolve.bind(this);
+        this.onReject = this.onReject.bind(this);
+        executorFunction(this.onResolve, this.onReject);
+    }
+
+    then(handleSuccess){
+        this.promiseChain.push(handleSuccess);
+        return this;
+    }
+
+    catch(handleError){
+        this.handleError = handleError;
+    }
+
+    onResolve(value){
+
+        let currentValue = value;
+        try{
+            this.promiseChain.forEach(promise =>{
+                currentValue = promise(currentValue);
+            })
+        }catch(err){
+            this.onReject(err);
+        }
+    }
+
+    onReject(error){
+        this.handleError(error);
+    }
+
+    
+}
+
+myFetch()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
