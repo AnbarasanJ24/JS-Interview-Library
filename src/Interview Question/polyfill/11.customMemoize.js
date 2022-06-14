c29355626764e18a04f8528f4dc4f3d708d02d37
Anbarasan <anbarasanj24@gmail.com>
2022-06-14 22:21:36 +0530

    Added Memoize Polyfill

--- src/Interview Question/polyfill/11.customMemoize.js
+++ src/Interview Question/polyfill/11.customMemoize.js
@@ -1,4 +1,19 @@
+// It will take a function and return memoized version of it
+// The input and output will be function but output with more power
+// create a cache and check the function argument present in it or not
+// If it is already cached then return cache value else save it in the cache
 
+function memoize(callbackFn){
+  let cache = {};
+  return function(){
+    let currentKey = JSON.stringify(arguments);
+    if(cache[currentKey]){
+      return cache[currentKey];
+    }
+    cache[currentKey] = callbackFn.apply(this, arguments);
+    return cache[currentKey];
+  }
+}
 
 function factorial(n) {
     if(n === 0 || n === 1) {
@@ -6,6 +21,15 @@ function factorial(n) {
     }
     return factorial(n-1) * n; 
  }
- console.log(factorial(25))
- 
+
+ const memoizedFactorial = memoize(factorial);
+ console.time('First');
+ console.log( memoizedFactorial(10));
+ console.timeEnd('First');
+ console.time('Second');
+ console.log( memoizedFactorial(10));
+ console.timeEnd('Second');
+ console.time('Third');
+ console.log( memoizedFactorial(10));
+ console.timeEnd('Third');
  
