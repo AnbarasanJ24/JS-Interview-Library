7196a4357328444ed9523d094cd456ed9ca0fd94
Anbarasan <anbarasanj24@gmail.com>
2022-07-24 10:50:33 +0530

    Added Promise All

--- src/Interview Question/polyfill/promises/3. customPromiseAll.js
+++ src/Interview Question/polyfill/promises/3. customPromiseAll.js
@@ -1,3 +1,9 @@
+/*
+    It takes array of promises and return a promise that resolve a array of results 
+    Once all the promise is resolved then return the result
+    If any one of the promise is reject then it will return error
+ */
+
 function task(time){
 
     return new Promise((resolve, reject)=>{
@@ -7,29 +13,32 @@ function task(time){
     })
 }
 
-const taskList = [task(1000), task(5000), task(3000)];
+const promises = [task(1000), task(5000), task(3000)];
 
 function myPromiseAll(promises) {
-
     let result = [];
     let promiseCompleted = 0;
-
-    return new Promise((resolve, reject) => {
-
-        promises.forEach((promise, index) => {
-            promise
-                .then((value) => {
-                    result[index]= value;
-                    promiseCompleted++;
-
-                    if(promiseCompleted === promises.length){
-                        resolve(result);
-                    }
-                })
-                .catch(err => reject(err));
-        });
+  
+    return new Promise((resolve, reject)=>{
+  
+      if(!promises.length){
+        return resolve(promises);
+      }
+  
+      promises.forEach((promise,index) =>{
+        Promise.resolve(promise)
+          .then(value =>{
+            result[index] = value;
+            promiseCompleted++;
+  
+            if(promiseCompleted === promises.length){
+              resolve(result);
+            }
+          })
+          .catch(err => reject(err))
+      })
     })
-}
+  }
 
 
 //run promise.all
@@ -38,3 +47,4 @@ myPromiseAll(taskList)
         console.log("got results", results)
     })
     .catch(console.error)
+
