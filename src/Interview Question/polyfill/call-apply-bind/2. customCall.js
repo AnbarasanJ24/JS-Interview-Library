86dce978d7968c375f88e467ab288c90923e09e3
Anbarasan <anbarasanj24@gmail.com>
2022-07-25 07:53:42 +0530

    Added Custom Call

--- src/Interview Question/polyfill/call-apply-bind/2. customCall.js
+++ src/Interview Question/polyfill/call-apply-bind/2. customCall.js
@@ -14,16 +14,31 @@ let ram = {
     age: 22,
 }
 
-// Steps to produce call
-// 1. First attach the function needs to be called in the context, function available as this keyword
-// 2. After it is attached, call the function with passed parameter
+/*
+    Approach : Basic call function will take thisArgs and ...args and it will call a function with those values
+    Function (displayname), we need to invoke will be "this" keyword inside custom call,
+    In order to handle null, undefined we just check thisArgs, If anything is null or undefined use window object
+    There is a possibility of sending primitive value as thisArgs, so convert everything to object using Object(context)
 
-Function.prototype.customCall = function (context, ...args) {
-    let ram = context;
-    ram.displayName = this;
+    Now to call a function on context object, we need to first attach the function inside it. context = > { func : displayname}
+    To reduce name coliision, create unique name using symbol() and attach the function context => {func1234 : displayname}
+    Now call the function with args and delete the identifier after it 
+*/
 
-    ram.displayName(...args);
+Function.prototype.customCall = function(thisArg, ...args) {
+    const func = this;
+    let context = thisArg ?? window;
+    context = Object(context);
+    
+    // Attach the function inside the context
+    const identifier = Symbol('func');
+    context[identifier] = func;
+    const response = context[identifier](...args);
+    delete context[identifier];
+  
+    return response;
 }
+
 
 // anbu.displayname.call(ram);
 anbu.displayname.customCall(ram, 'Pondicherry');
